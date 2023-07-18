a293a2627163570741b5d9865fe0fde4d1852e4d
EdwinRuiz <koppox@gmail.com>
2023-07-17 22:33:58 -0500

    Update files

--- webserver/main.py
+++ webserver/main.py
@@ -1,5 +1,22 @@
+import store
 from fastapi import FastAPI
-from fastapi.response import HTMLResponse
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/about', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Contact Page</h1>
+        <p>Parraph first</p>
+    """
+
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
